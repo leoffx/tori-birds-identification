73cafee6eda7c241d1c37b5402fc34342e9cf4cb
unknown <leoff152@gmail.com>
2020-06-25 15:28:44 +0200

    Revert "Updated Harpia model"
    This reverts commit bff3465267fe19650b559b7c0fe599cdb2a535cf.

--- src/app/services/model.service.ts
+++ src/app/services/model.service.ts
@@ -1,26 +1,28 @@
-import { Injectable } from "@angular/core";
+import { Injectable } from '@angular/core';
 
 declare const tf;
 
 @Injectable({
-  providedIn: "root",
+  providedIn: 'root'
 })
 export class TfService {
+
   public model: any;
 
   init() {
-    const MODEL_URL = "assets/tf/model.json";
-    tf.loadGraphModel(MODEL_URL).then((res) => (this.model = res));
+    const MODEL_URL = 'assets/tf/model.json';
+    tf.loadGraphModel(MODEL_URL).then(res => this.model = res);
   }
 
   public async predict(foto): Promise<any> {
     return await tf.tidy(() => {
-      const fotoTensor = tf.browser
-        .fromPixels(foto)
-        .resizeBilinear([224, 224])
-        .asType("float32");
+      const fotoTensor = tf.browser.fromPixels(foto)
+        .resizeNearestNeighbor([224, 224])
+        .asType('float32');
 
-      const fotoNormalizada = fotoTensor.div([255]).expandDims();
+      const fotoNormalizada = fotoTensor
+        .div([255])
+        .expandDims();
 
       return this.model.predict(fotoNormalizada);
     });
