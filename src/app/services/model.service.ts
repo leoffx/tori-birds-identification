0165231ec57ba296024ddb0fbdfed43379345a9b
unknown <leoff152@gmail.com>
2020-06-25 14:22:35 +0200

    Added GH pages support

--- src/app/services/model.service.ts
+++ src/app/services/model.service.ts
@@ -8,18 +8,23 @@ declare const tf;
 export class TfService {
   public model: any;
 
-  init() {
-    const MODEL_URL = "assets/tf/model.json";
-    tf.loadGraphModel(MODEL_URL).then((res) => (this.model = res));
+  public init(): Promise<any> {
+    const MODEL_URL = "../../assets/model.json";
+    return tf.loadGraphModel(MODEL_URL).then((res) => {
+      this.model = res;
+      console.log("initialized model:");
+      console.log(res);
+    });
   }
 
   public async predict(foto): Promise<any> {
+    console.log("starting prediction:");
+    console.log(this.model);
     return await tf.tidy(() => {
       const fotoTensor = tf.browser
         .fromPixels(foto)
         .resizeBilinear([224, 224])
         .asType("float32");
-
       const fotoNormalizada = fotoTensor.div([255]).expandDims();
 
       return this.model.predict(fotoNormalizada);
